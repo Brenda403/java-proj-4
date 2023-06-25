e6c069ff5b5e82e25aae97e48c062595c102aeab
Brenda <73964041+Brenda403@users.noreply.github.com>
2023-06-25 19:00:59 -0400

    initial commit

---
+++ starter/src/main/java/com/devmountain/noteApp/dtos/NoteDto.java
@@ -0,0 +1,29 @@
+package com.devmountain.noteApp.dtos;
+
+import com.devmountain.noteApp.entities.Note;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collector;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoteDto {
+    private Long id;
+    private String body;
+    private UserDto userDto;
+
+    public NoteDto(Note note) {
+        if (note.getId() != null) {
+            this.id = note.getId();
+        }
+        if (note.getBody() != null) {
+            this.body = note.getBody();
+        }
+    }
+
+}
+
